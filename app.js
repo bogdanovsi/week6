713df41c6fb6123e66a1518056ae8054a978bd41
bogdanovsi <bogdanovsi884@gmail.com>
2021-01-02 14:26:56 +0300

    cors

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
+import mongoose from 'mongoose'
+import p from 'puppeteer'
+
 export default (express, bodyParser, createReadStream, crypto, http) => {
     const app = express()
     const moduleUrl = import.meta.url.substring(7)
+
     app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
+
+    app.use((req, res, next) => {
+        res.append('Access-Control-Allow-Origin', '*');
+        res.append('Access-Control-Allow-Headers', '*');
+        res.append('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,OPTIONS,DELETE');
+        res.set('Access-Control-Expose-Headers', '*');
+        next();
+    })
+
+    app.get('/test/', async (req, res) => {
+        const URL = req.query.URL;
+        const browser = await p.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] })
+        const page = await browser.newPage();
+        await page.goto(URL);
+        await page.waitForSelector('#bt');
+        await page.click('#bt')
+        const got = await page.$eval('#inp', el => el.value);
+        browser.close();
+        res.end(got)
+    })
 
     app.get('/login/', (req, res) => {
         res.end('itmo287662')
@@ -11,7 +36,7 @@ export default (express, bodyParser, createReadStream, crypto, http) => {
         createReadStream(moduleUrl).pipe(res)
     })
 
-    app.get('/sha1/:input', (req, res) => {
+    app.get('/sha1/:input/', (req, res) => {
         const input = req.params.input;
         var shasum = crypto.createHash('sha1')
         shasum.update(input)
@@ -19,10 +44,12 @@ export default (express, bodyParser, createReadStream, crypto, http) => {
         return res.end(hex)
     })
 
-    app.all('/req/', (req, res) => {
-        console.log(req.query.addr);
-        console.log(req.body)
+    app.post('/insert/', (req, res) => {
+        console.log(req.body);
+        res.end(JSON.stringify(req.body))
+    })
 
+    app.all('/req/', (req, res) => {
         let reqURL = req.query.addr;
         if (reqURL == null) {
             reqURL = req.body.addr
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import {createReadStream} from 'fs'
+import { createReadStream } from 'fs'
 import crypto from 'crypto'
 import http from 'http'
 
